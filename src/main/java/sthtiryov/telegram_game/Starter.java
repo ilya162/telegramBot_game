5b6bde507a45d255b0dc6884e9aeed0253ffc82b
Ilya <dj-nfb@mail.ru>
2023-10-20 17:13:44 +0300

    Create Bot

---
+++ src/main/java/sthtiryov/telegram_game/Starter.java
@@ -0,0 +1,9 @@
+package sthtiryov.telegram_game;
+
+import com.pengrad.telegrambot.TelegramBot;
+
+public class Starter {
+    public static void main(String[] args) {
+        new Bot().serve();
+    }
+}
---
+++ src/test/java/com/example/telegram_game/TelegramGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
